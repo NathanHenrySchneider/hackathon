35c9f216c2bd3d1f672a606cd8f79f7bc6be5b89
Paschal A <radzlaif@gmail.com>
2020-07-28 20:28:34 -0400

    Just some pseudocode about what could be.

---
+++ server.py
@@ -0,0 +1,10 @@
+"""
+class server
+
+static User pseudoUser
+Map<string, object> userKeyPair = new Map<>();
+
+Iforget what should come next
+perhaps some keey exchange
+
+"""
---
+++ user.py
@@ -0,0 +1,23 @@
+"""
+class user
+
+static string userName;
+static Key userKey;
+
+user(userName, userKey){
+    this.userName = userName
+    this.userKey = userKey
+}
+
+static giveKey(){
+
+}
+
+static saveKey(){
+    
+}
+
+
+
+
+"""
