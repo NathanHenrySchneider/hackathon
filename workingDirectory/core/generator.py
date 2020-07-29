64673d65c24003f9b7e14dfe3b46b2a5eb49059c
Dakota <dv185083@ncr.com>
2020-07-29 10:57:52 -0400

    Now generating 128 char ascii strings

--- workingDirectory/core/generator.py
+++ workingDirectory/core/generator.py
@@ -1,16 +1,19 @@
 import time
 import math
 import random
+from rulesets import RULESETS
 
  #000, 001, 010, 011, 100, 101, 110, 111
 
 # class Generator():
 
+SIZE = 1024
+ITERATIONS = 10
 
-arrayLength = 20
+arrayLength = SIZE
 dataCurrent = []
 dataPrevious = []
-ruleSet = "01011000"
+ruleSet = ''
 
 def generate_initial_seed():
     global arrayLength
@@ -26,50 +29,67 @@ def generate_initial_seed():
 def bitWiseCalc(neighborhood, index):
     global dataCurrent
     neighborhood = neighborhood << 1
-    #print("shift:" +str(neighborhood))
     neighborhood = neighborhood & 7
-    #print("and7: " + str(neighborhood))
-    #print("index: " + str(index))
-    #print("currind: " + str(dataCurrent[index]))
     if (index != arrayLength):
-        # print("index: " + str(index))
-        # print("currind: " + str(dataCurrent[index]))
         neighborhood = neighborhood | dataCurrent[index]
-    #print("nieghCalc: " + str(neighborhood))
-    #print("neighborhood:" + str(neighborhood) + "\n")
     return neighborhood
 
 def arrayAnalysis():
-    global dataCurrent
+    global dataCurrent, ruleSet
     count = 0
     neighborhood = int( ("0" + str(dataCurrent[0]) + str(dataCurrent[1])) , 2)
-    #print("start: " + str(neighborhood))
     arr = [0] * arrayLength
-    #arr[0] = int(ruleSet [neighborhood])
     index = 2
     while(index < arrayLength + 1):
-        #print("ruleSet: " + (ruleSet [neighborhood]))
-        arr[count] = int(ruleSet [neighborhood])
+        arr[count] = int(ruleSet[neighborhood])
         neighborhood = bitWiseCalc(neighborhood, index)
-        #print("while: " + str(neighborhood))
         count += 1
         index += 1
-    arr[count] = int(ruleSet [neighborhood])
+    arr[count] = int(ruleSet[neighborhood])
     dataPrevious = dataCurrent
     dataCurrent = arr
 
 
-
 def generatorCycle():
-    while(True):
-    #for i in range(20):
-        #print(dataCurrent)
-        prnt()
+    #while(True):
+    global ruleSet
+    changeRuleSet()
+    #printRulesAndData()
+    for i in range(ITERATIONS):
+        #printX()
+        print_ASCII()
         arrayAnalysis()
-    prnt()
+        changeRuleSet()
+        #printRulesAndData()
+    #printX()
+    print_ASCII()
     #print(dataCurrent)
 
-def prnt():
+def changeRuleSet():
+    global ruleSet
+    ruleSet = RULESETS[random.randint(0, len(RULESETS) - 1)]
+
+def translate_to_ASCII(binary_string):
+    ascii_list = [binary_string[i:i+8] for i in range(0, len(binary_string), 8)]
+    #print(binary_string)
+    #print(ascii_list)
+    ascii_string = []
+    for char_list in ascii_list:
+        string = ''
+        for char in char_list:
+            string += str(char)
+        num = int(string, 2) % 127
+        num = num if num > 32 else num + 33
+        ascii_string.append(chr(num))
+    return ''.join(ascii_string)
+
+def printRulesAndData():
+    global ruleSet
+    global dataCurrent
+    print(dataCurrent, end='\n\n')
+    print(ruleSet)
+
+def printX():
     global dataCurrent
     n = 0
     str = ""
@@ -81,17 +101,17 @@ def prnt():
         n += 1
     print(str)
 
-def __init__(length):
-    #print("VGUHB JHUNHs")
+def print_ASCII():
+    global dataCurrent
+    print(translate_to_ASCII(dataCurrent))
+
+def init(length):
     arrayLength = length
     generate_initial_seed()
-    #print("VGUHB JHUNHs")
     generatorCycle()
 
 def main():
-    #print("yugvbhjuibjn")
-    __init__(20)
-    #print("UYUHUUJHUIJUI")
+    init(SIZE)
 
 if __name__ == "__main__":
     main()
